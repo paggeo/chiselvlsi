361e3113c2f489b44f7e192d19358381e7a085a3
paggeo <georgepag4028@gmail.com>
2023-06-21 15:35:10 +0300

    the 1,2 bits does not work

---
+++ assignment2/src/test/scala/multpipetester.scala
@@ -0,0 +1,31 @@
+package adder
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+import adder._
+
+class multpipetester extends AnyFlatSpec with ChiselScalatestTester{
+  "Multpipetester" should "pass" in {
+    test(new multpipe){ dut =>
+      var step = 0;
+      for (a <- 3 to 3){
+        for (b <- 1 to 10){
+            dut.io.A.poke(a.U)
+            dut.io.B.poke(b.U)
+            dut.clock.step(1)
+            val product = dut.io.P.peek().litValue
+            println(s"step: $step, a: $a, b: $b, product: $product")
+            step = step + 1 
+        }
+      }
+      for (_ <- 0 to 8){
+        dut.clock.step(1)
+        val product = dut.io.P.peek().litValue
+        println(s"step: $step, product: $product")
+        step = step + 1 
+      }
+
+    }
+  }
+}
